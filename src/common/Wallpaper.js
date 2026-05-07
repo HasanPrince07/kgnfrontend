afcaa83d5a0c658f32f6c40b04a71211c33259b8
Mohammad Hasan <131869132+HasanPrince07@users.noreply.github.com>
2026-05-07 17:01:49 +0530

    Update Wallpaper.js

--- src/common/Wallpaper.js
+++ src/common/Wallpaper.js
@@ -1,5 +1,3 @@
-const BASE_URL = process.env.REACT_APP_API_BASE_URL;
-
 function Wallpaper({ heading, imgSrc }) {
     return (
         <>
@@ -7,7 +5,7 @@ function Wallpaper({ heading, imgSrc }) {
                 <div className="container-fluid">
                     <div className="row">
                         <div className="col-12 outer-div px-sm-3 px-2 pt-sm-3 pt-2 position-relative">
-                            <img className="w-100 h-100 object-fit-cover rounded-2" src={`${BASE_URL}/${imgSrc}`} alt="not-found" fetchPriority="high" />
+                            <img className="w-100 h-100 object-fit-cover rounded-2" src={imgSrc} alt="not-found" fetchPriority="high" />
                             <div className="inner-div position-absolute top-0 bottom-0 start-0 end-0 d-flex align-items-center mx-sm-3 mx-2 mt-sm-3 mt-2 rounded-2">
                                 <h2 className="fw-bold text-uppercase m-0 px-sm-5 px-3">{heading}</h2>
                             </div>
