8c7b8b2c1ab014771ba0cfd25b7f90cddee24012
Mohammad Hasan <131869132+HasanPrince07@users.noreply.github.com>
2026-05-11 11:15:21 +0530

    Update History.js

--- src/admin/History.js
+++ src/admin/History.js
@@ -109,6 +109,7 @@ function History() {
     }
 
     const handleDelete = async () => {
+        setLoading(prev => ({ ...prev, form: true }));
         try {
             const res = await fetch(`${BASE_URL}/admin/deletehistory/${id}`, {
                 credentials: "include"
@@ -125,6 +126,8 @@ function History() {
         } catch (error) {
             toast("Network error, please check your internet", { type: "error" });
             console.log("Error during delete history data:", error);
+        } finally {
+            setLoading(prev => ({ ...prev, form: false }));
         }
     }
 
@@ -242,7 +245,7 @@ function History() {
                                 <h3 className="text-center text-uppercase fw-bold mb-sm-4 mb-3">Are you sure you want to delete?</h3>
                                 <div className="d-flex">
                                     <button onClick={() => handleDeleteModal(false)} className="btn form-control text-uppercase rounded-0 shadow-none fw-bold mx-2">cancel</button>
-                                    <button onClick={handleDelete} className="btn form-control text-uppercase rounded-0 shadow-none fw-bold mx-2">delete</button>
+                                    <button onClick={handleDelete} disabled={loading.form} className="btn form-control text-uppercase rounded-0 shadow-none fw-bold mx-2">{loading.form ? <><div className="spinner mx-auto"></div></> : "delete"}</button>
                                 </div>
                             </div>
                         </div>
